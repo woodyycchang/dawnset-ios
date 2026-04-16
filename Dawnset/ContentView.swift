c3746901c102cdff3f5c2f4dbb4b951d23171ce8
Woody Chang <yc2998@cornell.edu>
2026-04-16 19:27:46 -0400

    Initial Commit

---
+++ Dawnset/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dawnset
+//
+//  Created by WOODY CHANG on 4/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dawnset/DawnsetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DawnsetApp.swift
+//  Dawnset
+//
+//  Created by WOODY CHANG on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct DawnsetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
